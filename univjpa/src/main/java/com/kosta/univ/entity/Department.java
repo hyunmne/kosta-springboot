1d954120f54f6ac5cfc670bac740ec4abda47e80
hyunmne <echo0913@naver.com>
2024-04-26 14:31:22 +0900

    Springboot-univJPA: entity 관계 설정

---
+++ univjpa/src/main/java/com/kosta/univ/entity/Department.java
@@ -0,0 +1,44 @@
+package com.kosta.univ.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer deptno;
+	@Column
+	private String dname;
+	@Column
+	private Integer part;
+	@Column
+	private String build;
+	
+	@OneToMany(mappedBy="deptno1", fetch=FetchType.LAZY)
+	private List<Student> stdList1 = new ArrayList<>();
+	
+	@OneToMany(mappedBy="deptno2", fetch=FetchType.LAZY)
+	private List<Student> stdList2 = new ArrayList<>();
+}
---
+++ univjpa/src/main/java/com/kosta/univ/entity/Student.java
@@ -0,0 +1,64 @@
+package com.kosta.univ.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer studno;
+	
+	@Column
+	private String name;
+	@Column
+	private String id;
+	@Column
+	private Integer grade;
+	@Column
+	private String jumin;
+	@Column
+	private Date birhtday;
+	@Column
+	private String tel;
+	@Column
+	private Integer height;
+	@Column
+	private Integer weight;
+	@Column
+	private Integer profno;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="profno")
+	private Professor professor;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="deptno1")
+	private Department department1;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="deptno2")
+	private Department department2;
+}
