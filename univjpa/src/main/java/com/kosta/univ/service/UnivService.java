7b9184182971dea2fc67a52f6098f566c58f0659
hyunmne <echo0913@naver.com>
2024-04-29 19:19:44 +0900

    SprintBoot-univJPA: service

--- univjpa/src/main/java/com/kosta/univ/service/UnivService.java
+++ univjpa/src/main/java/com/kosta/univ/service/UnivService.java
@@ -3,6 +3,7 @@ package com.kosta.univ.service;
 import java.util.List;
 
 import com.kosta.univ.dto.DepartmentDto;
+import com.kosta.univ.dto.ProfessorDto;
 import com.kosta.univ.dto.StudentDto;
 import com.kosta.univ.entity.Department;
 import com.kosta.univ.entity.Professor;
@@ -13,49 +14,50 @@ public interface UnivService {
 	// 뷰서 등록
 	void saveDepartment(DepartmentDto deptDto) throws Exception;
 	void saveStudent(StudentDto stdDto) throws Exception;
+	void saveProfessor(ProfessorDto profDto) throws Exception;
 	
 	// 학생
 	// 학생 이름으로 학생 목록 조회
-	List<Student> stdListByName(String studName) throws Exception;
+	List<StudentDto> stdListByName(String studName) throws Exception;
 	// 제 1전공으로 학생 목록 조회
-	List<Student> stdListInDept1ByDeptName(String dept1) throws Exception;
-	List<Student> stdListInDept1ByDeptNo(Integer dept1) throws Exception;
+	List<StudentDto> stdListInDept1ByDeptName(String dept1) throws Exception;
+	List<StudentDto> stdListInDept1ByDeptNo(Integer dept1) throws Exception;
 	// 제 2전공으로 학생 목록 조회
-	List<Student> stdListInDeptByDeptName(String dept2) throws Exception;
-	List<Student> stdListInDept2ByDeptNo(Integer dept2) throws Exception;
+	List<StudentDto> stdListInDeptByDeptName(String dept2) throws Exception;
+	List<StudentDto> stdListInDept2ByDeptNo(Integer dept2) throws Exception;
 	// 학년으로 학생 목록 조회
-	List<Student> stdListByGrade(Integer grade) throws Exception;
+	List<StudentDto> stdListByGrade(Integer grade) throws Exception;
 	// 담당 교수가 없는 학생 목록 조회
-	List<Student> stdListByNoProf() throws Exception;
+	List<StudentDto> stdListByNoProf() throws Exception;
 	// 학번으로 학생 조회
-	Student stdByStudNo(Integer studNo) throws Exception;
+	StudentDto stdByStudNo(Integer studNo) throws Exception;
 	// 주민번호로 학생 조회
-	Student stdByJumin(String jumin) throws Exception;
+	StudentDto stdByJumin(String jumin) throws Exception;
 	
 	
 	// 교수
 	// 교수 번호로 담당 학생 목록 조회
-	List<Student> stdListByProfNo(Integer profNo) throws Exception;
+	List<StudentDto> stdListByProfNo(Integer profNo) throws Exception;
 	//교수번호로 교수 조회
-	Professor profByProfNo(Integer profNo) throws Exception;
+	ProfessorDto profByProfNo(Integer profNo) throws Exception;
 	// 교수 이름으로 교수 목록 조회
-	List<Professor> profListByProfName(String profName) throws Exception;
+	List<ProfessorDto> profListByProfName(String profName) throws Exception;
 	// 학과 번호로 교수목록 조회
-	List<Professor> profListByDeptNo(Integer deptno) throws Exception;
+	List<ProfessorDto> profListByDeptNo(Integer deptno) throws Exception;
 	// 학과 이름으로 교수 목록 조회
-	List<Professor> profListByDeptName(String deptName) throws Exception;
+	List<ProfessorDto> profListByDeptName(String deptName) throws Exception;
 	// 직급으로 교수 목록 조회
-	List<Professor> profListByPosition(String position) throws Exception;
+	List<ProfessorDto> profListByPosition(String position) throws Exception;
 	
 	// 학과
 	// 학과 번호로 학과 조회
-	Department deptByDeptNo(Integer deptNo) throws Exception;
+	DepartmentDto deptByDeptNo(Integer deptNo) throws Exception;
 	// 학과 이름으르로 학과 조회
-	Department deptByDeptName(String dpetName) throws Exception;
+	DepartmentDto deptByDeptName(String dpetName) throws Exception;
 	// 학과 편집
-	List<Department> deptListByPart(String part) throws Exception;
+	List<DepartmentDto> deptListByPart(String part) throws Exception;
 	// 위치하는 건물로 학과 목록 조회
-	List<Department> deptListByBuild(String build) throws Exception;
+	List<DepartmentDto> deptListByBuild(String build) throws Exception;
 	
 	
 }
--- univjpa/src/main/java/com/kosta/univ/service/UnivServiceImpl.java
+++ univjpa/src/main/java/com/kosta/univ/service/UnivServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kosta.univ.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +9,7 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import com.kosta.univ.dto.DepartmentDto;
+import com.kosta.univ.dto.ProfessorDto;
 import com.kosta.univ.dto.StudentDto;
 import com.kosta.univ.entity.Department;
 import com.kosta.univ.entity.Professor;
@@ -30,143 +32,210 @@ public class UnivServiceImpl implements UnivService {
 	private final StudentRepository studentRepository;
 
 	@Override  //학생 이름으로 학생목록 조회
-	public List<Student> stdListByName(String studName) throws Exception {
-		return studentRepository.findByName(studName);
+	public List<StudentDto> stdListByName(String studName) throws Exception {
+		List<StudentDto> studDtoList = new ArrayList<>();
+		List<Student> studList = studentRepository.findByName(studName);
+		for (Student std : studList) {
+			studDtoList.add(std.toDto());
+		}
+		return studDtoList;
 	}
 
 	@Override  //제1전공으로 학생목록 조회
-	public List<Student> stdListInDept1ByDeptName(String deptName) throws Exception {
+	public List<StudentDto> stdListInDept1ByDeptName(String deptName) throws Exception {
 		Optional<Department> odept = departmentRepository.findByDname(deptName);
+		List<StudentDto> stdDtoList = null;
 		if(odept.isPresent()) {
-			return odept.get().getStdList1();
+				stdDtoList = new ArrayList<>();
+			for(Student std : odept.get().getStdList1()) {
+				stdDtoList.add(std.toDto());
+			}
 		}
-		return null;
+		return stdDtoList;
 	}
 
 	@Override
-	public List<Student> stdListInDept1ByDeptNo(Integer deptNo) throws Exception {
-		return studentRepository.findByDepartment1Deptno(deptNo);
+	public List<StudentDto> stdListInDept1ByDeptNo(Integer deptNo) throws Exception {
+		List<StudentDto> stdDtoList = new ArrayList<StudentDto>();
+		List<Student> stdList = studentRepository.findByDepartment1Deptno(deptNo);
+		for (Student std : stdList) {
+			stdDtoList.add(std.toDto());
+		}
+		return stdDtoList;
 	}
 
 	@Override  //제2전공으로 학생목록 조회
-	public List<Student> stdListInDeptByDeptName(String deptName) throws Exception {
+	public List<StudentDto> stdListInDeptByDeptName(String deptName) throws Exception {
 		Optional<Department> odept = departmentRepository.findByDname(deptName);
 		if(odept.isPresent()) {
-			return odept.get().getStdList2();
+			List<StudentDto> stdDtoList = new ArrayList<>();
+			for (Student std : odept.get().getStdList2()) {
+				stdDtoList.add(std.toDto());
+			}
 		}
 		return null;
 	}
 
 	@Override
-	public List<Student> stdListInDept2ByDeptNo(Integer deptNo) throws Exception {
-		return studentRepository.findByDepartment2Deptno(deptNo);
+	public List<StudentDto> stdListInDept2ByDeptNo(Integer deptNo) throws Exception {
+		List<StudentDto> stdDtoList = new ArrayList<StudentDto>();
+		List<Student> stdList = studentRepository.findByDepartment2Deptno(deptNo);
+		for (Student std : stdList) {
+			stdDtoList.add(std.toDto());
+		}
+		return stdDtoList;
 	}
 
 	@Override  //학년으로 학생목록 조회
-	public List<Student> stdListByGrade(Integer grade) throws Exception {
-		return studentRepository.findByGrade(grade);
+	public List<StudentDto> stdListByGrade(Integer grade) throws Exception {
+		List<StudentDto> stdDtoList = new ArrayList<StudentDto>();
+		List<Student> stdList = studentRepository.findByGrade(grade);
+		for (Student std : stdList) {
+			stdDtoList.add(std.toDto());
+		}
+		return stdDtoList;
 	}
 
 	@Override  //담당교수가 없는 학생목록 조회
-	public List<Student> stdListByNoProf() throws Exception {
-		return studentRepository.findByProfessorIsNull();
+	public List<StudentDto> stdListByNoProf() throws Exception {
+		List<StudentDto> stdDtoList = new ArrayList<StudentDto>();
+		List<Student> stdList = studentRepository.findByProfessorIsNull();
+		for (Student std : stdList) {
+			stdDtoList.add(std.toDto());
+		}
+		return stdDtoList;
 	}
 
 	@Override  //학번으로 학생 조회
-	public Student stdByStudNo(Integer studno) throws Exception {
+	public StudentDto stdByStudNo(Integer studno) throws Exception {
 		Optional<Student> ostud = studentRepository.findById(studno);
-		if(ostud.isPresent()) return ostud.get();
+		if(ostud.isPresent()) return ostud.get().toDto();
 		return null;
 	}
 
 	@Override  //주민번호로 학생 조회
-	public Student stdByJumin(String jumin) throws Exception {
+	public StudentDto stdByJumin(String jumin) throws Exception {
 		Optional<Student> ostud = studentRepository.findByJumin(jumin);
-		if(ostud.isPresent()) return ostud.get();
+		if(ostud.isPresent()) return ostud.get().toDto();
 		return null;
 	}
 
 	@Override // 교수 번호로 학생 목록 조회
-	public List<Student> stdListByProfNo(Integer profNo) throws Exception {
+	public List<StudentDto> stdListByProfNo(Integer profNo) throws Exception {
 		Optional<Professor> oprof = professorRepository.findById(profNo);
 		if(oprof.isPresent()) {
-			return oprof.get().getStdList();
+			List<StudentDto> stdDtoList = new ArrayList<>();
+			for (Student std : oprof.get().getStdList()) {
+				stdDtoList.add(std.toDto());
+			}
 		}
 		return null;
 	}
 
 	@Override
-	public Professor profByProfNo(Integer profNo) throws Exception {
+	public ProfessorDto profByProfNo(Integer profNo) throws Exception {
 		Optional<Professor> oprof = professorRepository.findById(profNo);
-		if(oprof.isPresent()) return oprof.get();
+		if(oprof.isPresent()) return oprof.get().toDto();
 		return null;
 	}
 
 	@Override
-	public List<Professor> profListByProfName(String profName) throws Exception {
-		return professorRepository.findByName(profName);
+	public List<ProfessorDto> profListByProfName(String profName) throws Exception {
+		List<ProfessorDto> profDtoList = new ArrayList<>();
+		List<Professor> profList = professorRepository.findByName(profName);
+		for(Professor prof : profList) {
+			profDtoList.add(prof.toDto());
+		}
+		return profDtoList;
 	}
 
-	@Override
-	public List<Professor> profListByDeptNo(Integer deptno) throws Exception {
+	@Override // 학과번호으로 교수 리스트 조회
+	public List<ProfessorDto> profListByDeptNo(Integer deptno) throws Exception {
 		Optional<Department> odept = departmentRepository.findById(deptno);
 		if(odept.isPresent()) {
-			return odept.get().getProfList();
+			List<ProfessorDto> profDtoList = new ArrayList<>();
+			for(Professor prof : odept.get().getProfList()) {
+				profDtoList.add(prof.toDto());
+			}
 		}
 		return null;
 	}
 
-	@Override
-	public List<Professor> profListByDeptName(String deptName) throws Exception {
+	@Override // 학과명으로 교수 목록 조회
+	public List<ProfessorDto> profListByDeptName(String deptName) throws Exception {
 		Optional<Department> odept = departmentRepository.findByDname(deptName);
 		if(odept.isPresent()) {
-			return odept.get().getProfList();
+			List<ProfessorDto> profDtoList = new ArrayList<>();
+			for(Professor prof : odept.get().getProfList()) {
+				profDtoList.add(prof.toDto());
+			}
 		}
 		return null;
 	}
 
 	@Override
-	public List<Professor> profListByPosition(String position) throws Exception {
-		return professorRepository.findByPosition(position);
+	public List<ProfessorDto> profListByPosition(String position) throws Exception {
+		List<ProfessorDto> profDtoList = new ArrayList<ProfessorDto>();
+		List<Professor> profList = professorRepository.findByPosition(position);
+		for(Professor prof : profList) {
+			profDtoList.add(prof.toDto());
+		}
+		return profDtoList;
 	}
 
 	@Override
-	public Department deptByDeptNo(Integer deptNo) throws Exception {
+	public DepartmentDto deptByDeptNo(Integer deptNo) throws Exception {
 		Optional<Department> odept = departmentRepository.findById(deptNo);
-		if(odept.isPresent()) return odept.get();
+		if(odept.isPresent()) return odept.get().toDto();
 		return null;
 	}
 
 	@Override
-	public Department deptByDeptName(String dpetName) throws Exception {
+	public DepartmentDto deptByDeptName(String dpetName) throws Exception {
 		Optional<Department> odept = departmentRepository.findByDname(dpetName);
-		if(odept.isPresent()) return odept.get();
+		if(odept.isPresent()) return odept.get().toDto();
 		return null;
 	}
 
 	@Override
-	public List<Department> deptListByPart(String part) throws Exception {
-		return departmentRepository.findByPart(part);
+	public List<DepartmentDto> deptListByPart(String part) throws Exception {
+		List<DepartmentDto> deptDtoList = new ArrayList<>();
+		List<Department> deptList = departmentRepository.findByPart(part);
+		for(Department dept : deptList) {
+			deptDtoList.add(dept.toDto());
+		}
+		return deptDtoList; 
 	}
 
 	@Override
-	public List<Department> deptListByBuild(String build) throws Exception {
-		return departmentRepository.findByBuild(build);
+	public List<DepartmentDto> deptListByBuild(String build) throws Exception {
+		List<DepartmentDto> deptDtoList = new ArrayList<>();
+		List<Department> deptList = departmentRepository.findByBuild(build);
+		for(Department dept : deptList) {
+			deptDtoList.add(dept.toDto());
+		}
+		return deptDtoList; 
 	}
 
 	@Override
 	public void saveDepartment(DepartmentDto deptDto) throws Exception {
-		System.out.println(deptDto);
-		Department dept = deptByDeptNo(deptDto.getDeptno());
-		if (dept!=null) throw new Exception("등록된 학과번호입니다.");
+		Optional<Department> odept = departmentRepository.findById(deptDto.getDeptno());
+		if (odept!=null) throw new Exception("등록된 학과번호입니다.");
 		departmentRepository.save(deptDto.toEntity());
 	}
 	
 	@Override
 	public void saveStudent(StudentDto stdDto) throws Exception {
-		Student std = stdByStudNo(stdDto.getStudno());
-		if (std!=null) throw new Exception("등록된 학번입니다.");
+		Optional<Student> ostd = studentRepository.findById(stdDto.getStudno());
+		if (ostd!=null) throw new Exception("등록된 학번입니다.");
 		studentRepository.save(stdDto.toEntity());
+	}
+
+	@Override
+	public void saveProfessor(ProfessorDto profDto) throws Exception {
+		Optional<Professor> oprof = professorRepository.findById(profDto.getProfno());
+		if(oprof!=null)  throw new Exception("등록된 교수번호입니다.");
+		professorRepository.save(profDto.toEntity());
 	}
 
 }
