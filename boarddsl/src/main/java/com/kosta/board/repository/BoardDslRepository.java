be7b59061132710c2534143930daec01cac721d0
hyunmne <echo0913@naver.com>
2024-04-30 17:49:42 +0900

    Springboot-boardDSL: 조회, 페이징, 검색

--- boarddsl/src/main/java/com/kosta/board/repository/BoardDslRepository.java
+++ boarddsl/src/main/java/com/kosta/board/repository/BoardDslRepository.java
@@ -1,12 +1,90 @@
 package com.kosta.board.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 
+import com.kosta.board.entity.Board;
+import com.kosta.board.entity.QBoard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Repository
 public class BoardDslRepository {
 	@Autowired
-	private JPAQueryFactory jpqQueryFactory;
+	private JPAQueryFactory jpaQueryFactory;
+	
+	@Autowired
+	private BoardRepository brdres;
+	
+	// 글 작성
+	public void insertBoard(Board brd) {
+		brdres.save(brd);
+	}
+	
+	// 상세조회
+	public List<Board> findBrdListByPaging(PageRequest pageRequest) throws Exception {
+		QBoard brd = QBoard.board;
+		return jpaQueryFactory.selectFrom(brd)
+				              .orderBy(brd.num.desc()) // 정렬순서
+				              .offset(pageRequest.getOffset()) // ~부터
+				              .limit(pageRequest.getPageSize()) // ~개
+							  .fetch();
+	}
+	
+	public Long findbrdcnt() throws Exception {
+		QBoard brd = QBoard.board;
+		return jpaQueryFactory.select(brd.count())
+							  .from(brd)
+							  .fetchOne();
+	}
+	
+	public List<Board> searchBrdListByPaging(PageRequest page, String word, String type) throws Exception {
+		QBoard brd = QBoard.board;
+		if (type.equals("subject")) {
+			return jpaQueryFactory.selectFrom(brd)
+					.where(brd.subject.contains(word))
+					.orderBy(brd.num.desc()) // 정렬순서
+					.offset(page.getOffset()) // ~부터
+					.limit(page.getPageSize()) // ~개
+					.fetch();
+		} else if (type.equals("content")) {
+			return jpaQueryFactory.selectFrom(brd)
+					.where(brd.content.contains(word))
+					.orderBy(brd.num.desc()) // 정렬순서
+					.offset(page.getOffset()) // ~부터
+					.limit(page.getPageSize()) // ~개
+					.fetch();
+		} else if(type.equals("writer")) {
+			return jpaQueryFactory.selectFrom(brd)
+					.where(brd.writer.eq(word))
+					.orderBy(brd.num.desc()) // 정렬순서
+					.offset(page.getOffset()) // ~부터
+					.limit(page.getPageSize()) // ~개
+					.fetch();
+			
+		} else return null;
+	}
+	
+	public Long searchBrdCnt(String type, String word) {
+		QBoard brd = QBoard.board;
+		if (type.equals("subject")) {
+			return jpaQueryFactory.select(brd.count())
+								  .where(brd.subject.contains(word))
+								  .from(brd)
+								  .fetchOne();
+		} else if (type.equals("content")) {
+			return jpaQueryFactory.select(brd.count())
+						     	  .where(brd.content.contains(word))
+						     	  .from(brd)
+						     	  .fetchOne();
+		} else if(type.equals("writer")) {
+			return jpaQueryFactory.select(brd.count())
+								  .where(brd.writer.contains(word))
+					              .from(brd)
+					              .fetchOne();
+			
+		} else return 0L;
+	}
 }
--- boarddsl/src/main/java/com/kosta/board/service/BoardServiceImpl.java
+++ boarddsl/src/main/java/com/kosta/board/service/BoardServiceImpl.java
@@ -1,26 +1,83 @@
 package com.kosta.board.service;
 
+import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kosta.board.dto.BoardDto;
+import com.kosta.board.entity.BFile;
+import com.kosta.board.entity.Board;
+import com.kosta.board.repository.BoardDslRepository;
+import com.kosta.board.repository.BoardLikeRepository;
+import com.kosta.board.repository.BoardRepository;
+import com.kosta.board.repository.FileRepository;
+import com.kosta.board.repository.MemberRepository;
 import com.kosta.board.util.PageInfo;
 
-@Service
-public class BoardServiceImpl implements BoardService {
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+	
+	@Value("${upload.path}")
+	private String uploadPath;
+	
+	private final FileRepository fileres;
+	private final BoardDslRepository brddslres;
+	private final BoardLikeRepository brdlikeres;
+	private final MemberRepository memres;
+	private final BoardRepository brdres;
+	
 	@Override
 	public void boardWrite(BoardDto boardDto, MultipartFile file) throws Exception {
-		// TODO Auto-generated method stub
-
+		if(file!=null && !file.isEmpty()) {
+			BFile bFile = BFile.builder()
+							.name(file.getOriginalFilename())
+							.directory(uploadPath)
+							.size(file.getSize())
+							.contenttype(file.getContentType())
+							.build();
+			
+			fileres.save(bFile); // DB에 file 저장
+			
+			File upFile = new File(uploadPath, bFile.getNum()+"");
+			file.transferTo(upFile); // 폴더에 파일 업로드 
+			boardDto.setFileNum(bFile.getNum());
+		}
+		
+		brdres.save(boardDto.toBoard()); // board table에 글 삽입 
 	}
 
 	@Override
 	public List<BoardDto> boardListByPage(PageInfo pageInfo, String type, String word) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		PageRequest pageRequest = PageRequest.of(pageInfo.getCurPage()-1, 10); // repository -> offset, limit
+		
+		List<Board> brdList = null; // 페이징작업
+		Long allCount = 0L;
+		
+		if(word==null || word.trim().equals("")) {
+			brdList = brddslres.findBrdListByPaging(pageRequest); // 페이징작업
+			allCount = brddslres.findbrdcnt();
+		} else {
+			brdList = brddslres.searchBrdListByPaging(pageRequest, word, type);
+			allCount = brddslres.searchBrdCnt(type, word);
+		}
+		
+		int allPage = (int)(Math.ceil(allCount.doubleValue()/pageRequest.getPageSize()));
+		int startPage = (pageInfo.getCurPage()-1)/10*10+1;
+		int endPage = Math.min(startPage+10-1, allPage);
+		
+		pageInfo.setAllPage(allPage);
+		pageInfo.setStartPage(startPage);
+		pageInfo.setEndPage(endPage);
+		
+		return brdList.stream().map(Board::toBoardDto).collect(Collectors.toList());
 	}
 
 	@Override
