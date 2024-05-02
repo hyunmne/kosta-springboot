ecbbe88f8f7480d58de218efd98548b7f82be8b3
hyunmne <echo0913@naver.com>
2024-05-02 11:17:46 +0900

    SpringBoot-BoardDSL:로그인 회원가입 중복체크

--- boarddsl/src/main/java/com/kosta/board/repository/MemberRepository.java
+++ boarddsl/src/main/java/com/kosta/board/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.kosta.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kosta.board.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
+	Optional<Member> findByNickname(String nickname);
 }
--- boarddsl/src/main/java/com/kosta/board/service/MemberServiceImpl.java
+++ boarddsl/src/main/java/com/kosta/board/service/MemberServiceImpl.java
@@ -1,34 +1,49 @@
 package com.kosta.board.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kosta.board.dto.MemberDto;
+import com.kosta.board.entity.Member;
+import com.kosta.board.repository.MemberRepository;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
+	@Autowired
+	private MemberRepository memres;
+	
 	@Override
 	public void join(MemberDto mem) throws Exception {
-		// TODO Auto-generated method stub
-
+		Optional<Member> omem = memres.findById(mem.getId());
+		if(omem.isPresent()) throw new Exception("아이디 중복 오류");
+		memres.save(mem.toEntity());
 	}
 
 	@Override
 	public MemberDto login(String id, String password) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Member> omem = memres.findById(id);
+		if(omem.isEmpty()) throw new Exception("아이디 오류");
+		
+		Member member = omem.get();
+		if(!member.getPassword().equals(password.trim())) {
+			throw new Exception ("비밀번호 오류");
+		}
+		return member.toDto();
 	}
 
 	@Override
 	public Boolean checkDoubleId(String id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Member> omem = memres.findById(id);
+		return omem.isPresent();
 	}
 
 	@Override
 	public Boolean checkDoubleNickName(String nickName) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Member> omem = memres.findByNickname(nickName);
+		return omem.isPresent();
 	}
 
 }
