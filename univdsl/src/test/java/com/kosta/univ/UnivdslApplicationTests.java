60d053ea56d2aff341aabf24d7f8ed4408ce4a60
hyunmne <echo0913@naver.com>
2024-05-02 18:11:50 +0900

    SpringBoot-UnivDSL: test

--- univdsl/src/test/java/com/kosta/univ/UnivdslApplicationTests.java
+++ univdsl/src/test/java/com/kosta/univ/UnivdslApplicationTests.java
@@ -1,13 +1,62 @@
 package com.kosta.univ;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kosta.univ.dto.StudentDto;
+import com.kosta.univ.service.UnivService;
 
 @SpringBootTest
 class UnivdslApplicationTests {
 
-//	@Test
-	void contextLoads() {
+	@Autowired
+	private UnivService uniservice;
+	
+	@Test
+	void getStdListByName() {
+		try {
+			List<StudentDto> stdDto = uniservice.getStdListByName("서재수");
+			System.out.println(stdDto);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
+	
+	@Test
+	void getStdListByDeptNo1Or2() {
+		try {
+			List<StudentDto> stdDto = uniservice.getStdListByDeptNo1Or2(100);
+			System.out.println(stdDto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void getStdListByDName1Or2() {
+		try {
+			List<StudentDto> stdDto = uniservice.getStdListByDName1Or2("컴퓨터공학부", "전자공학과");
+			System.out.println(stdDto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void getStdListByDName1Or2_2() {
+		try {
+			List<StudentDto> stdDto = uniservice.getStdListByDName1Or2("컴퓨터공학부");
+			System.out.println(stdDto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 
 }
