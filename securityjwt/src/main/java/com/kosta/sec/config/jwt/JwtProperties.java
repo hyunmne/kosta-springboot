a6b83789e4e4ea0f45ebbee246b85c76ac11df9c
hyunmne <echo0913@naver.com>
2024-05-24 10:55:50 +0900

    개미친오타

--- securityjwt/src/main/java/com/kosta/sec/config/jwt/JwtProperties.java
+++ securityjwt/src/main/java/com/kosta/sec/config/jwt/JwtProperties.java
@@ -5,5 +5,5 @@ public interface JwtProperties {
 	int ACCESS_EXPIRATION_TIME = 60000*60*2;  // 2시간
 	int REFRESH_EXPIRATION_TIME = 60000*60*24; // 24시간
 	String TOKEN_PREFIX = "Bearer ";
-	String HEADER_STRING = "Aurhorization";
+	String HEADER_STRING = "Authorization";
 }
